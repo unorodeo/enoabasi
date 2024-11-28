c2aa2f0803e53f232baec0db330b8f4b881a6108
enoabasi <enoabasidream@gmail.com>
2024-11-28 17:49:03 +0100

    fix: import

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,13 +1,13 @@
 import localFont from "next/font/local";
 
 export const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
+	src: "../public/fonts/GeistVF.woff",
 	variable: "--font-geist-sans",
 	weight: "100 900",
 });
 
 export const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
+	src: "../public/fonts/GeistMonoVF.woff",
 	variable: "--font-geist-mono",
 	weight: "100 900",
 });
