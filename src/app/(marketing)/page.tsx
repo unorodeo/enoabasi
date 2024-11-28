df54f3ae0817559009b91440ac3f6504c5324eab
enoabasi <enoabasidream@gmail.com>
2024-11-28 23:32:17 +0100

    feat: home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,11 @@
+import { Section } from "@/components/ui/section";
+
 export default function Home() {
-	return <>Home</>;
+	return (
+		<>
+			<Section>
+				<h1>Welcome</h1>
+			</Section>
+		</>
+	);
 }
