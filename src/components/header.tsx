79de268acbd3c16ba6a30eba889359dc0fb46ad7
enoabasi <enoabasidream@gmail.com>
2024-11-29 00:43:42 +0100

    refac: remove `bg` styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Header: React.FC = () => {
 	return (
-		<header className="sticky top-0 inset-x-0 bg-background/60 supports-[backdrop-blur]:backdrop-blur-md">
+		<header className="sticky inset-x-0 top-0 z-50">
 			<nav className="flex items-center justify-between px-4 mx-auto max-w-7xl md:px-8 h-14">
 				<Brand variant="link" />
 				<Menu />
