f9b245310923b2ca81c7d8a1bd0834a4db1f41ca
enoabasi <enoabasidream@gmail.com>
2024-11-29 22:13:51 +0100

    feat: add `experiences` section
    - section on the page
    - experience components

---
+++ src/components/experience.tsx
@@ -0,0 +1,62 @@
+import { Experience as ExperienceType } from "@/types/globals";
+import React from "react";
+
+interface ExperienceItem extends ExperienceType {
+  id: string;
+}
+
+const EXPERIENCE_ITEMS: ExperienceItem[] = [
+  {
+    id: "6d31da73-591c-46da-b6c5-3f589c042b6d",
+    period: "2022 - Present",
+    position: "Software developer",
+    company: "Nobasi Softwares",
+    description:
+      "Started a company to build softwares that contributed to society. Developing solutions for public administrations using advanced technologies and best practices.",
+  },
+  {
+    id: "7d4abfd6-28f0-4fdc-a881-df8c2aa37a7a",
+    period: "2020 - 2021",
+    position: "Software developer intern",
+    company: "Ice Queen Spa",
+    description:
+      "My role as a software developer was to help them manage/maintain an exsiting website for their business. implement internal tools that helped with booking, managing, and scheduling customer appointments.",
+  },
+];
+
+export const Experiences: React.FC = () => (
+  <>
+    <ol className="relative border-s-2 border-border">
+      {EXPERIENCE_ITEMS.map((item) => (
+        <Experience
+          key={item.id}
+          period={item.period}
+          position={item.position}
+          company={item.company}
+          description={item.description}
+        />
+      ))}
+    </ol>
+  </>
+);
+
+export const Experience: React.FC<ExperienceType> = ({
+  period,
+  position,
+  company,
+  description,
+}) => {
+  return (
+    <li className="mb-10 ms-4">
+      <div className="absolute size-3 bg-muted rounded-full mt-1.5 -start-1.5 border border-muted"></div>
+      <time className="mb-1 text-sm font-normal leading-none text-muted-foreground font-geist-mono">
+        {period}
+      </time>
+      <h3 className="text-lg font-semibold tracking-normal">{position}</h3>
+      <small className="muted">{company}</small>
+      <p className="[&:not(:first-child)]:mt-0 mb-4 text-base font-normal text-foreground">
+        {description}
+      </p>
+    </li>
+  );
+};
--- src/types/globals.ts
+++ src/types/globals.ts
@@ -2,22 +2,44 @@
  * A navigation route link
  */
 export interface Navitem {
-	/**
-	 * The label of the link
-	 */
-	label: string;
-	/**
-	 * The route of the link
-	 */
-	href: `/${string}`;
+  /**
+   * The label of the link
+   */
+  label: string;
+  /**
+   * The route of the link
+   */
+  href: `/${string}`;
 }
 
 /**
  * A navigation route link with an icon
  */
 export interface NavitemWithIcon extends Navitem {
-	/**
-	 * The icon of the link
-	 */
-	icon: React.JSX.ElementType;
+  /**
+   * The icon of the link
+   */
+  icon: React.JSX.ElementType;
+}
+
+/** 
+ * An experience of work
+ */
+export interface Experience {
+  /**
+   * The length of work
+   */
+  period: `${string} - ${string}`;
+  /**
+   * The role played
+   */
+  position: string;
+  /**
+   * The place of work
+   */
+  company: string;
+  /**
+   * A brief of contribution
+   */
+  description: string;
 }
