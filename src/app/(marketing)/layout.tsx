679a00bff464c1ddcae7287c339b22e5e72c608f
enoabasi <enoabasidream@gmail.com>
2024-11-30 13:01:31 +0100

    fix(accessibility): remove unnecessary aria-role

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -10,7 +10,7 @@ export default function MarketingLayout({ children }: Props) {
 	return (
 		<main className="flex flex-col justify-between min-h-screen">
 			<Header />
-			<section role="main" className="relative flex-1">{children}</section>
+			<section className="relative flex-1">{children}</section>
 			<Footer />
 		</main>
 	);
