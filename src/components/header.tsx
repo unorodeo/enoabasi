4ad849090484d8611e8ba08998f077d46ac63317
enoabasi <enoabasidream@gmail.com>
2024-12-02 22:43:25 +0100

    update `header` styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export const Header: React.FC = () => {
 	return (
-		<header className="sticky inset-x-0 top-0 z-50">
-			<nav className="flex items-center justify-between px-4 mx-auto max-w-7xl md:px-8 h-14">
+		<header className="sticky inset-x-0 top-0 z-50 w-full">
+			<nav className="flex flex-row items-center justify-between px-4 mx-auto max-w-7xl md:px-8 h-14">
 				<Brand variant="link" />
 				<Menu />
 			</nav>
