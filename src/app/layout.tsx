686597bdb31ea7140a8611dc2dc75d5948414dec
enoabasi <enoabasidream@gmail.com>
2024-12-02 22:40:13 +0100

    chore: update -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,40 +7,32 @@ import { Providers } from "@/components/providers";
 import { cn } from "@/lib/cn";
 
 export const metadata: Metadata = {
-	title: "Enoabasi | The playground of Enoabasi Essien.",
-	description: "The playground of Enoabasi Essien.",
+  title: "Enoabasi | The playground of Enoabasi Essien.",
+  description: "The playground of Enoabasi Essien.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html
-			lang="en"
-			suppressHydrationWarning
-		>
-			<body
-				className={cn(
-					"antialiased font-geist-sans",
-					// selections
-					"selection:bg-tertiary selection:text-tertiary-foreground",
-					// custom scrollbar
-					// "[&::-webkit-scrollbar]:w-2",
-					// "[&::-webkit-scrollbar-track]:bg-muted",
-					// "[&::-webkit-scrollbar-thumb]:bg-muted-foreground",
-					// "[&::-webkit-scrollbar-thumb]:border-2",
-					// "[&::-webkit-scrollbar-thumb]:border-primary",
-					// "[&::-webkit-scrollbar-thumb]:rounded-lg",
-					geistSans.variable,
-					geistMono.variable,
-					calistoga.variable
-				)}
-			>
-				<Providers>{children}</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body
+        className={cn(
+          "relative antialiased font-geist-sans",
+          "selection:bg-tertiary selection:text-tertiary-foreground",
+          geistSans.variable,
+          geistMono.variable,
+          calistoga.variable
+        )}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
 
